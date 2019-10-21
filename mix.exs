692bcd904c9201276211b6fffb178849b6094d69
Jeonghan-Seok <rudebono@gmail.com>
2019-10-21 15:42:53 +0900

    Fix mix.exs for hex.pm
    Fix elixir version,
    Add package, description, ex_doc in deps.

--- mix.exs
+++ mix.exs
@@ -5,24 +5,40 @@ defmodule Bip39.MixProject do
     [
       app: :bip39,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:crypto]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description() do
+    "#Bitcoin #BIP39 #Mnemonic #Elixir"
+  end
+
+  defp package() do
+    [
+      name: :bip39,
+      files: ~w(lib priv .formatter.exs mix.exs README* LICENSE*),
+      maintainers: ["Jeonghan-Seok"],
+      licenses: ["MIT License"],
+      links: %{
+        "GitHub" => "https://github.com/Jeonghan-Seok/bip39",
+        "Docs" => "https://hexdocs.pm/bip39"
+      }
     ]
   end
 end
