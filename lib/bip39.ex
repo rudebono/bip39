4c060c7f0d5f741a10865e8dbd872ca086f8f4aa
Jeonghan-Seok <rudebono@gmail.com>
2019-10-21 11:47:53 +0900

    Add functions in Bip39
    Add functions in Bip39:
    
    - entropy_to_mnemonic/2
    - mnemonic_to_entropy/2
    - get_words/1

--- lib/bip39.ex
+++ lib/bip39.ex
@@ -1,18 +1,29 @@
 defmodule Bip39 do
-  @moduledoc """
-  Documentation for Bip39.
-  """
+  def entropy_to_mnemonic(entropy, words) do
+    checksum_length = (bit_size(entropy) / 32) |> trunc()
+    <<checksum::size(checksum_length), _::bitstring>> = :crypto.hash(:sha256, entropy)
+    entropy_with_checksum = <<entropy::bitstring, <<checksum::size(checksum_length)>>::bitstring>>
+    indexes = for <<index::11 <- entropy_with_checksum>>, do: index
+    mnemonic = Enum.map(indexes, fn index -> Enum.at(words, index) end)
+    mnemonic
+  end
 
-  @doc """
-  Hello world.
+  def mnemonic_to_entropy(mnemonic, words) do
+    checksum_length = (length(mnemonic) / 3) |> trunc()
+    entropy_length = length(mnemonic) * 11 - checksum_length
+    indexes = Enum.map(mnemonic, fn m_word -> Enum.find_index(words, &(&1 == m_word)) end)
 
-  ## Examples
+    <<entropy::size(entropy_length), _checksum::size(checksum_length)>> =
+      Enum.reduce(indexes, <<>>, fn index, bits ->
+        <<bits::bitstring, <<index::11>>::bitstring>>
+      end)
 
-      iex> Bip39.hello()
-      :world
+    <<entropy::size(entropy_length)>>
+  end
 
-  """
-  def hello do
-    :world
+  def get_words(lang) do
+    "priv/#{Atom.to_string(lang)}.txt"
+    |> File.stream!()
+    |> Enum.map(&String.trim/1)
   end
 end
