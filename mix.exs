61102e3ea38a689a579899e68f21f9e5564e81c8
Jeonghan-Seok <rudebono@gmail.com>
2019-10-21 10:28:07 +0900

    Add :crypto in mix.exs
    Add :crypto in mix.exs for :crypto.hash(:sha256, "")

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Bip39.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:crypto]
     ]
   end
 
